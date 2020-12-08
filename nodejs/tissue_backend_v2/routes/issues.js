9805845966ec42bc22a584601bbd882a0cfc3743
joshuamwolfe <joshuamwolfe@gmail.com>
2020-12-07 20:39:04 -0500

    finished module 7

--- nodejs/tissue_backend_v2/routes/issues.js
+++ nodejs/tissue_backend_v2/routes/issues.js
@@ -2,16 +2,22 @@ let express = require('express');
 let router = express.Router();
 let issueManager = require('../issue-manager/issue');
 const mongoose = require('mongoose');
-// const fakeArray = [...issueManager.issues()];
+
 
 const issue = mongoose.model('issue', new mongoose.Schema({    
     id: String,
     title: String,
     status: String,
-    created: String,
-    updated: String,
+    // created: String,
+    // updated: String,
     details: String,     
-}))
+},
+{ 
+    timestamps: { 
+      createdAt: 'Created', 
+      updatedAt: 'Updated' 
+    } 
+}));
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true}));
@@ -20,79 +26,64 @@ router.get('/issues', (req, res) => {
 	/*
 	Returns all issues as json
 	*/
-    issue.find()
+    issue.find({})
         .then(Model => res.json(Model));
     // res.json(fakeArray)
 });
 
 router.post('/issues', (req,res) => {
-	/* The HTTP reqeuest body contains the JSON with the new data for this isssue. In your code to handle this route, you should find the issue in your issues array with the 	correspoinding id and update it oaccordingly. */
-   var fakeArray =
-	{id: 1,
-	title: 'bad apple',
-	status: 'new',
-	created: '1/1/01',
-	updated: '',
-	details: 'Website broke?'};
-
-	// const response = req.body;
-	// const movie = issue.find(m => m.id === parseInt(req.params.id));
+    /* The HTTP reqeuest body contains the JSON with the new data for this isssue. In your code to handle this route, you should find the issue in your issues array with the correspoinding id and update it oaccordingly. */
+    const {id,title,status,details} = req.body; // look at deconstructing later TODO
+    var fakeArray =
+	{id: id,
+	title: title,
+	status: status,
+    details: details};
+    
     issue.create(fakeArray)
         .then(Model => res.json(Model));
         
-
-	// if (!movie) {
-	// 	res.status(404).send('The movie with the given ID was not found.');
-	// }
-	
-    // // movie = req.body;
-    // console.log('Movie: ', movie);
-	// res.send(movie);
 });
 
-router.put('/issues/issues.json', (req, res) => {	
-	/*
-	The HTTP Request body contains the JSON with
-	the data to add a new issue. In your code to
-	handle this route, you should retrieve the 
-	issue data and create a new Issue object and
-	store it in your issues array
-	*/
-	const response = req.body;
-	const newIssue = issueManager.create_issue(response);	
-	fakeArray.push(newIssue); //to make	
-	res.status(200).send('The information you asked for has been retrieved.')
+router.put('/issues/:id', (req, res) => {	
+    /*The HTTP Request body contains the JSON with the data to add a new issue. In your code to handle this route, you should retrieve the issue data and create a new Issue object and store it in your issues array*/    
+    const {id,title,status,details} = req.body;
+
+    issue.findOne({id:req.params.id}, function (err, issue) {
+        if(id !== undefined) {
+            issue.id = id;
+        }
+
+        if(title !== undefined) {
+            issue.title = title;
+        }
+
+        if(status !== undefined) {
+            issue.status = status;
+        }
+
+        if(details !== undefined) {
+            issue.details = details;
+        }
+
+        issue.save().then(dbTransaction => {
+            res.json(dbTransaction);
+          })
+          .catch(err => {
+            res.status(404).json(err);
+          });
+    })
+
 });
 
-router.post('issues/:id', (req,res) => {
+router.delete('/issues/:id', (req,res) => {
 	/*
 	Removes the issue from the issue array.
 	*/
 
-    // const movie = issue.find(m => m.id === parseInt(req.params.id));
-    
-	// if (!movie) {
-	// 	res.status(404).send('The course with the given ID was not found.')
-	// 	return
-	// }
-
-	// const index = issue.indexOf(movie);
-	// issu e.splice(index, 1);
-    // res.status(200).send('Movie deleted!');
     console.log('id: ', req.params.id);
     issue.deleteOne({ id: req.params.id})
         .then(dbModel => res.json(dbModel));
-    
-    // issue.findById({ _id: req.params.id })
-    //   .then(dbModel => dbModel.remove())
-    //   .then(dbModel => res.json(dbModel));
 });
-
-
-const getName = () => {
-    return 'Jim';
-  };
-
-exports.getName = getName;
 
 module.exports = router;
--- nodejs/tissue_backend_v2/routes/users.js
+++ nodejs/tissue_backend_v2/routes/users.js
@@ -1,57 +1,41 @@
 let express = require('express');
 let router = express.Router();
-const fakeArray = [];
-let nextId = 1;
+const mongoose = require('mongoose');
+let issueManager = require('../issue-manager/issue');
 
-class User {
-	constructor (firstName, lastName, email, cell, address) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.cell = cell;
-		this.address = address;
-		this.id = nextId;
-		++nextId;
-	}
-
-	createUser() {
-		//push user info to a user array
-	}
-
-	deleteUser() {
-		//delete user at given index
-	}
-
-	get user() {
-		//given the index retrieve user from the user array
-	}
-}
+const user = mongoose.model('user', new mongoose.Schema({    
+    firstName: String,
+    lastName: String,
+    email: String,
+    cell: String,
+    id: String
+}));
 
 router.get('/users', (req, res) => {
 	/*
 	Returns all users as json
 	*/
-	res.json(fakeArray)  
+    user.find({})
+    .then(Model => res.json(Model));
 });
 
 router.put('/users/users.json', (req,res) => {
 	/*
 	The HTTP request body contains the JSON with the data for the new user. In your code to handle this route, you should create a new User obect and and add it to a users array.
 	*/
+    const { firstName, lastName, email, cell, id} = req.body;
 
-	const response = req.body;
-	const newIssue = new User(response);
+    let fakeArray =
+	{
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        cell: cell,
+        id: id
+    };
 
-	fakeArray.push(newIssue);
-	res.status(200).send('User created!');
+    user.create(fakeArray)
+        .then(Model => res.json(Model));
 });
 
 module.exports = router;
-
-/* 
-
-Notes:
-
-
-
-*/
