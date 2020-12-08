27a21e169ac0320531eeedf53875440a438897cd
joshuamwolfe <joshuamwolfe@gmail.com>
2020-12-07 19:28:42 -0500

    backing up work

--- nodejs/tissue_backend_v2/routes/issues.js
+++ nodejs/tissue_backend_v2/routes/issues.js
@@ -4,15 +4,13 @@ let issueManager = require('../issue-manager/issue');
 const mongoose = require('mongoose');
 // const fakeArray = [...issueManager.issues()];
 
-const issue = mongoose.model('issue', new mongoose.Schema({
-    issue: {
-        Id: String,
-        Title: String,
-        Status: String,
-        Created: String,
-        Updated: String,
-        Details: String, 
-    }
+const issue = mongoose.model('issue', new mongoose.Schema({    
+    id: String,
+    title: String,
+    status: String,
+    created: String,
+    updated: String,
+    details: String,     
 }))
 
 router.use(express.json());
@@ -25,6 +23,31 @@ router.get('/issues', (req, res) => {
     issue.find()
         .then(Model => res.json(Model));
     // res.json(fakeArray)
+});
+
+router.post('/issues', (req,res) => {
+	/* The HTTP reqeuest body contains the JSON with the new data for this isssue. In your code to handle this route, you should find the issue in your issues array with the 	correspoinding id and update it oaccordingly. */
+   var fakeArray =
+	{id: 1,
+	title: 'bad apple',
+	status: 'new',
+	created: '1/1/01',
+	updated: '',
+	details: 'Website broke?'};
+
+	// const response = req.body;
+	// const movie = issue.find(m => m.id === parseInt(req.params.id));
+    issue.create(fakeArray)
+        .then(Model => res.json(Model));
+        
+
+	// if (!movie) {
+	// 	res.status(404).send('The movie with the given ID was not found.');
+	// }
+	
+    // // movie = req.body;
+    // console.log('Movie: ', movie);
+	// res.send(movie);
 });
 
 router.put('/issues/issues.json', (req, res) => {	
@@ -41,38 +64,7 @@ router.put('/issues/issues.json', (req, res) => {
 	res.status(200).send('The information you asked for has been retrieved.')
 });
 
-router.post('/issues', (req,res) => {
-	/*
-	The HTTP reqeuest body contains the JSON 
-	with the new data for this isssue. In your
-	code to handle this route, you should find
-	the issue in your issues array with the
-	correspoinding id and update it oaccordingly.
-    */
-   var fakeArray = [
-	{id: 1,
-	title: 'bad apple',
-	status: 'new',
-	created: '1/1/01',
-	updated: '',
-	details: 'Website broke?'}];
-
-	// const response = req.body;
-	// const movie = issue.find(m => m.id === parseInt(req.params.id));
-    issue.create(fakeArray[0])
-        .then(Model => res.json(Model));
-        
-
-	// if (!movie) {
-	// 	res.status(404).send('The movie with the given ID was not found.');
-	// }
-	
-    // // movie = req.body;
-    // console.log('Movie: ', movie);
-	// res.send(movie);
-});
-
-router.delete('issues/:id', (req,res) => {
+router.post('issues/:id', (req,res) => {
 	/*
 	Removes the issue from the issue array.
 	*/
@@ -85,9 +77,12 @@ router.delete('issues/:id', (req,res) => {
 	// }
 
 	// const index = issue.indexOf(movie);
-	// issue.splice(index, 1);
+	// issu e.splice(index, 1);
     // res.status(200).send('Movie deleted!');
     console.log('id: ', req.params.id);
+    issue.deleteOne({ id: req.params.id})
+        .then(dbModel => res.json(dbModel));
+    
     // issue.findById({ _id: req.params.id })
     //   .then(dbModel => dbModel.remove())
     //   .then(dbModel => res.json(dbModel));
